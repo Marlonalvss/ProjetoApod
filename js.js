3671a2f809d760913d45022af6332081003f8799
Marlon <marlon14br@gmail.com>
2022-05-25 14:59:57 -0300

    redirecionamento javascript

--- js.js
+++ js.js
@@ -1,7 +1,16 @@
-/*var pegabotao = document.getElementById("bt")
-pegabotao.addEventListener("click",somepage)
+$("#botao1").on('click', function () {
+    window.open("https://pt.wikipedia.org/wiki/Astronomy_Picture_of_the_Day")
+})
+$("#botao2").on('click', function () {
+    window.open("https://pt.wikipedia.org/wiki/Universo")
+})
+$("#botao3").on('click', function () {
+    window.open("https://www.linkedin.com/in/marlon-alves-92b96a17b/")
+})
+$("#botao4").on('click', function () {
+    window.open("https://github.com/Marlonalvss")
+}) 
 
-function somepage(){
-    var pegadisplay = document.getElementById("teste")
-    pegadisplay.style.display = ("none");
-}*/
+$("#explorar").on('click', function () {
+    window.location.href = "./2janela/selecione.html"
+}) 
