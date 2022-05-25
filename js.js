95ddbba57b87e1afde2427a34bc33648f09f7ba2
Marlon <marlon14br@gmail.com>
2022-05-24 23:55:33 -0300

    index e pesquisa

---
+++ js.js
@@ -0,0 +1,7 @@
+/*var pegabotao = document.getElementById("bt")
+pegabotao.addEventListener("click",somepage)
+
+function somepage(){
+    var pegadisplay = document.getElementById("teste")
+    pegadisplay.style.display = ("none");
+}*/
